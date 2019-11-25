4975951ffb53783e2030d7b1391e0bc8fbb879b7
Will <comfortwb21@gmail.com>
2019-11-25 07:56:44 -0500

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,6 @@ import PetBrowser from './PetBrowser'
 class App extends React.Component {
   constructor() {
     super()
-
     this.state = {
       pets: [],
       filters: {
@@ -14,6 +13,27 @@ class App extends React.Component {
       }
     }
   }
+ 
+  fetchPets = () => {
+    let url = '/api/pets'
+    this.state.filters.type === 'all' ? url : url = `/api/pets?type=${this.state.filters.type}`
+    fetch(url)
+      .then(res => res.json())
+      .then(p => this.setState({pets: p}))
+  }
+
+  updateFilter = data => {
+    this.setState({filters: {type: data}}) 
+  }
+
+  onAdoptPet = (petId) => {
+    let pet = this.state.pets.map(p => {
+      return p.id === petId ? {...p, isAdopted: true} : p
+    })
+    this.setState({pets: pet})
+  }
+
+
 
   render() {
     return (
@@ -24,10 +44,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.updateFilter} onFindPetsClick={this.fetchPets}/>
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} onAdoptPet={this.onAdoptPet}/>
             </div>
           </div>
         </div>
--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,12 +1,22 @@
 import React from 'react'
 
 class Filters extends React.Component {
+
+  onSelect = (event) => {
+    let selection = event.target.value 
+    this.props.onChangeType(selection)
+  }
+
+  clickFind = (event) => {
+    this.props.onFindPetsClick(event)
+  }
+
   render() {
     return (
       <div className="ui form">
         <h3>Animal type</h3>
         <div className="field">
-          <select name="type" id="type">
+          <select name="type" id="type" onChange={this.onSelect}>
             <option value="all">All</option>
             <option value="cat">Cats</option>
             <option value="dog">Dogs</option>
@@ -15,7 +25,7 @@ class Filters extends React.Component {
         </div>
 
         <div className="field">
-          <button className="ui secondary button">Find pets</button>
+          <button className="ui secondary button" onClick={this.clickFind}>Find pets</button>
         </div>
       </div>
     )
